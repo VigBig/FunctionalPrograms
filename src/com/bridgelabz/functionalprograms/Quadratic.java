a63f5460d59c965022215e9abf461604e1bf10ad
VighneshBilgi <vhbilgi@gmail.com>
2022-07-07 00:07:48 +0530

    Added: Quadratic.java

--- src/com/bridgelabz/functionalprograms/Quadratic.java
+++ src/com/bridgelabz/functionalprograms/Quadratic.java
@@ -17,11 +17,17 @@ public class Quadratic {
         System.out.println("Enter C value:");
         int c = sc.nextInt();
 
+        quadraticRoots(a,b,c);
+
+
+    }
+
+    private static void quadraticRoots(int a, int b, int c) {
+
         int delta = b*b - 4*a*c;
         double root1 = (-b + Math.sqrt(delta))/(2*a);
         double root2 = (-b - Math.sqrt(delta))/(2*a);
 
         System.out.println("The Quadratic roots of equation "+a+"(x^2) +"+b+"(x) +"+c+" are : " + root1 +" and "+root2);
-
     }
 }
