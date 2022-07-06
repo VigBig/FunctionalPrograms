7400e59768cf7e6ce0a3da192e363c4da147a5be
VighneshBilgi <vhbilgi@gmail.com>
2022-07-06 19:50:23 +0530

    Added: Quadratic.java

---
+++ src/com/bridgelabz/functionalprograms/Quadratic.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.functionalprograms;
+
+import java.util.Scanner;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Roots (A,B,C) of Equation A(x^2) +B(x) +C  :");
+
+        System.out.println("Enter A value:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter B value:");
+        int b = sc.nextInt();
+
+        System.out.println("Enter C value:");
+        int c = sc.nextInt();
+
+        int delta = b*b - 4*a*c;
+        double root1 = (-b + Math.pow(delta,0.5))/(2*a);
+        double root2 = (-b - Math.pow(delta,0.5))/(2*a);
+
+        System.out.println("The Quadratic roots of equation "+a+"(x^2) +"+b+"(x) +"+c+" are : " + root1 +" and "+root2);
+
+    }
+}
