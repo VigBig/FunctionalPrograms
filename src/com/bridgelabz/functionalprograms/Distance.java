592f64264899ca927152f3917804c4639687e9e6
VighneshBilgi <vhbilgi@gmail.com>
2022-07-07 00:05:29 +0530

    Added: Distance.java

--- src/com/bridgelabz/functionalprograms/Distance.java
+++ src/com/bridgelabz/functionalprograms/Distance.java
@@ -9,6 +9,11 @@ public class Distance {
         int y = Integer.parseInt(args[1]);
         System.out.println("Y value is :"+y);
 
+        euclideanDistanceCalculator(x,y);
+    }
+
+    private static void euclideanDistanceCalculator(int x, int y) {
+
         double euclideanDistance = Math.pow((Math.pow(x,2)+Math.pow(y,2)),0.5);
 
         System.out.println("Euclidean Distance between (" + x + "," + y +") and (0,0) is :" + euclideanDistance);
